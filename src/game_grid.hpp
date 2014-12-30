5ffe9dccebfca5b3c1fc9e4ee84504e14d5ea4ea
moisesjbc <moisesjbc@gmail.com>
2014-12-30 20:57:13 +0000

    Added license advice to source code.

--- src/game_grid.hpp
+++ src/game_grid.hpp
@@ -1,4 +1,21 @@
-// TODO: Add license note.
+/*
+    Copyright 2009, 2014 Moises J. Bonilla Caraballo
+
+    This file is part of Tronmoi.
+
+    Tronmoi is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Tronmoi is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Tronmoi.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef GAME_GRID_HPP
 #define GAME_GRID_HPP
